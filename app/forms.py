69c6a2202f184875d4e2d4607a33957174080898
Alexandr <ieaiaio@bk.ru>
2019-12-16 13:51:27 +0800

    rename Module FlaskForm

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,20 @@
 from datetime import datetime
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, InputRequired
 from flask_ckeditor import CKEditorField
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField(validators=[DataRequired()], render_kw={"placeholder": "Логин"})
     password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Пароль"})
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
 
 
-class PostForm(Form):
+class PostForm(FlaskForm):
     title = StringField(validators=[InputRequired()], render_kw={"placeholder": "Заголовок"})
     body = CKEditorField('Body', validators=[DataRequired()])
     # timestamp = DateField(format='%Y-%m-%d', default=datetime.today)
