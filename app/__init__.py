2480f31ee90c401d48e1792b441e6c3fb5d1bd40
Alexandr <ieaiaio@bk.ru>
2020-02-04 18:20:24 +0800

    add chosen_v1.8.7 plugin

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from config import DevelopmentConfig
+from config import DevelopmentConfig, ProductionConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+app.config.from_object(ProductionConfig)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
