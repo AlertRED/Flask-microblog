a3335e27c54645ea1da5e5ea2ed6efbb36e06631
Alexandr <ieaiaio@bk.ru>
2020-02-25 13:23:57 +0800

    include support.js into base.html

--- microblog.py
+++ microblog.py
@@ -1,22 +1,4 @@
 from app import app
-app.run()
-# ### ADMIN ###
-#
-# from app.models import *
-# from flask_admin import Admin
-# from flask_admin.contrib.sqla import ModelView
-# from flask_login import current_user
-#
-#
-# class AdminMixin():
-#     def is_accessible(self):
-#         return current_user.is_authenticated and current_user.username == 'ieaiaio'
-#
-#
-# class AdminView(AdminMixin, ModelView):
-#     pass
-#
-#
-# admin = Admin(app)
-# admin.add_view(AdminView(User, db.session))
-# admin.add_view(AdminView(Post, db.session))
+
+if __name__ == "__main__":
+    app.run()
