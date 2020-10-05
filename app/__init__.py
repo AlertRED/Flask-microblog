c6f2b3f2ff3c94d86085a23423e01d96b636ce1a
Aleksandr <drammtv@gmail.com>
2020-10-06 02:03:33 +0300

    disable secret variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from config import DevelopmentConfig, ProductionConfig
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +9,9 @@ from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 app.config.from_object(ProductionConfig)
+print(os.environ)
+# postgres://dwetqmnbbarnpg:201161c755cbab58edc7e671d1d9f9273e6f6ea2997c3684582299e71f0b6f8a@ec2-174-129-18-98.compute-1.amazonaws.com:5432/ddd1jtv2tt3601
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import os
+
 class Config(object):
-    SECRET_KEY = 'qwertyuiopqwertyuiopqwertyuiopqwertyuiopww'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CKEDITOR_SERVE_LOCAL = True
     CKEDITOR_HEIGHT = 100
@@ -10,15 +11,18 @@ class Config(object):
     USER_UNAUTHENTICATED_ENDPOINT = 'login'
 
 
+
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@127.0.0.1/microblog'
     ENV = 'DevelopmentConfig'
     DEBUG = True
     TESTING = True
+    SECRET_KEY = 'qwerty'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@127.0.0.1/microblog'
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgres://dwetqmnbbarnpg:201161c755cbab58edc7e671d1d9f9273e6f6ea2997c3684582299e71f0b6f8a@ec2-174-129-18-98.compute-1.amazonaws.com:5432/ddd1jtv2tt3601"
     ENV = 'ProductionConfig'
     DEBUG = False
     TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
