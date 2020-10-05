7d8d077875559aebc61071e58d3a2ed88a3f3691
Aleksandr <drammtv@gmail.com>
2020-10-06 02:03:33 +0300

    disable secret variables

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
 class Config(object):
-    SECRET_KEY = 'qwertyuiopqwertyuiopqwertyuiopqwertyuiopww'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CKEDITOR_SERVE_LOCAL = True
     CKEDITOR_HEIGHT = 100
@@ -8,17 +12,17 @@ class Config(object):
     CKEDITOR_ENABLE_CODESNIPPET = True
     USER_EMAIL_SENDER_EMAIL = 'drammtv@gmail.com'
     USER_UNAUTHENTICATED_ENDPOINT = 'login'
+    SECRET_KEY = config.get('ACCESSES', 'SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('ACCESSES', 'DATABASE_URI')
 
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@127.0.0.1/microblog'
     ENV = 'DevelopmentConfig'
     DEBUG = True
     TESTING = True
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgres://dwetqmnbbarnpg:201161c755cbab58edc7e671d1d9f9273e6f6ea2997c3684582299e71f0b6f8a@ec2-174-129-18-98.compute-1.amazonaws.com:5432/ddd1jtv2tt3601"
     ENV = 'ProductionConfig'
     DEBUG = False
     TESTING = False
